c711b4b17155124f5d97e6d79f722b6adaf7dd74
tiago.eem@gmail.com <Tiagoeem@users.noreply.github.com>
2022-09-14 23:44:19 -0300

    ajustes nas funcoes de rotas

--- src/app.py
+++ src/app.py
@@ -27,17 +27,17 @@ def rota_buscar_registrado_por_ean(ean):
     cmd_sql = 'select * from tbl_registrados WHERE ean = ?'
     registrado, sucesso = select_query(cmd_sql, ean)
 
-    print(registrado)
+    resp = {'uid':registrado[0], 'ean':registrado[1], 'name':registrado[2]}
 
     if sucesso:
-        return jsonify(registrado), 200
+        return jsonify(resp), 200
     else:
         return {'erro':'Produto não encontrado nos registros'}, 404
 
 
 
-@app.route("/registrado/", methods=['POST'])
-def rota_inserir_prod_no_registro():
+@app.route("/registrado/<string:ean>", methods=['POST'])
+def rota_inserir_prod_no_registro(ean):
 
     try:
         content = request.get_json()
@@ -45,11 +45,12 @@ def rota_inserir_prod_no_registro():
         return {'erro':'Erro ao parsear o Json'}, 400
 
     cmd_sql = 'select * from tbl_registrados WHERE ean = ?'
-    registrado, encontrado = select_query(cmd_sql, content['ean'])
+    registrado, encontrado = select_query(cmd_sql, ean)
 
     if not(encontrado):
+        dado = {'ean': ean, 'name':content['name']}
         cmd_sql = 'insert into tbl_registrados(ean, name) values (?, ?)'
-        sucesso = insert_query(cmd_sql, content)
+        sucesso = insert_query(cmd_sql, dado)
         if sucesso:
             return jsonify({'mensagem':f'Produto {content["name"]} registrado com sucesso'}), 201
         else:
@@ -58,8 +59,8 @@ def rota_inserir_prod_no_registro():
         return {'erro':'Produto já existe no registro'}, 400
     
         
-@app.route("/registrado/", methods=['PUT'])
-def rota_update_prod_no_registro():
+@app.route("/registrado/<string:ean>", methods=['PUT'])
+def rota_update_prod_no_registro(ean):
 
     try:
         content = request.get_json()
@@ -67,11 +68,12 @@ def rota_update_prod_no_registro():
         return {'erro':'Erro ao parsear o Json'}, 400
 
     cmd_sql = 'select * from tbl_registrados WHERE ean = ?'
-    registrado, encontrado = select_query(cmd_sql, content['ean'])
+    registrado, encontrado = select_query(cmd_sql, ean)
 
     if encontrado:
+        dado = {'ean': ean, 'name':content['name']}
         cmd_sql = 'update tbl_registrados set name = ? WHERE ean = ?'
-        linhas_afetadas, sucesso = update_query(cmd_sql, content)
+        linhas_afetadas, sucesso = update_query(cmd_sql, dado)
         if sucesso:
             return jsonify({'mensagem':f'{linhas_afetadas} foram atualizadas com sucesso'}), 200
         else:
